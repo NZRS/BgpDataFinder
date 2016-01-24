1dd3d7efb5f140bd4d6077f994eb354633e8d1f2
Sebastian Castro <sebastian@nzrs.net.nz>
2016-01-24 14:11:50 +1300

    Redoing the rules to identify sources for PCH

--- PchBgpDataFinder.py
+++ PchBgpDataFinder.py
@@ -19,20 +19,32 @@ class PchBgpDataFinder(BgpDataFinder):
     url_stash = defaultdict(list)
 
     def __init__(self):
-        self.base_url = "https://www.pch.net/resources/Routing_Data/"
+        self.base_url = "https://www.pch.net/resources/Routing_Data" \
+                        "/IPv4_daily_snapshots/"
 
     def findsources(self):
         response = urllib2.urlopen(self.base_url)
         html = BeautifulSoup(response.read(), self.bs4_parser)
-        for link in html.findAll('a', href=re.compile('^\d{4}\/$')):
-            # PCH has a two level structure, let's dig a little bit
-            response2 = urllib2.urlopen(urlparse.urljoin(self.base_url, link[
-                'href']))
-            subcat = BeautifulSoup(response2.read(), self.bs4_parser)
-            for link2 in subcat.findAll('a', text=re.compile('\.pch\.net')):
-                source_url = urlparse.urljoin(response2.geturl(), link2['href'])
-                self.sources.append(source_url)
-                self.url_stash[link['href'][0:4]].append(source_url)
+        self.debug("1: %s" % response.geturl())
+        # Find all the links corresponding to the year
+        for link in html.findAll('a', href=re.compile('^201[6-9]/$')):
+            # PCH has a three level structure, let's dig a little bit
+            l2_link = urlparse.urljoin(self.base_url, link['href'])
+            response2 = urllib2.urlopen(l2_link)
+            self.debug("2: %s" % response2.geturl())
+            month = BeautifulSoup(response2.read(), self.bs4_parser)
+            for link2 in month.findAll('a', href=re.compile('^\d{2}/$')):
+                l3_link = urlparse.urljoin(l2_link, link2['href'])
+                response3 = urllib2.urlopen(l3_link)
+                self.debug("3: %s" % response3.geturl())
+                collector = BeautifulSoup(response3.read(), self.bs4_parser)
+                for link3 in collector.findAll('a',
+                                               href=re.compile('route\-collector\.\w{3}\.pch\.net/$')):
+                        source_url = urlparse.urljoin(response3.geturl(),
+                                                      link3['href'])
+                        self.sources.append(source_url)
+                        self.debug("*: %s" % source_url)
+                        self.url_stash[link['href'][0:4]].append(source_url)
 
         return self.sources
 
@@ -44,7 +56,7 @@ class PchBgpDataFinder(BgpDataFinder):
         # If the year requested is not available, return an empty list
         source_sublist = self.url_stash.get(self.today.strftime("%Y"), [])
 
-        file_pat = "\.pch\.net\.%s\.gz$" % self.today.strftime("%Y.%m.%d")
+        file_pat = "ipv4_bgp_routes\.%s\.gz$" % self.today.strftime("%Y.%m.%d")
         self.debug("Using pattern %s" % file_pat)
         for source in source_sublist:
             src_url = source
